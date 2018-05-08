07fa01073310582dbf01dd46c75a08928a7a4771
Jonathan Abantao <jonabantao@gmail.com>
2018-05-07 20:09:58 -0700

    Complete up to building basic UsersController

---
+++ W2D1/spring-boot-monolith/src/main/resources/db/migration/afterMigrate.sql
@@ -0,0 +1,7 @@
+INSERT INTO USERS
+	(USER_NAME, FIRST_NAME, LAST_NAME)
+VALUES
+	('user from seeds', 'Test', 'Er'),
+	('another user', 'Jon', 'Ab'),
+	('someone', 'Jack', 'Jill'),
+	('someone else', 'John', 'Doe');
