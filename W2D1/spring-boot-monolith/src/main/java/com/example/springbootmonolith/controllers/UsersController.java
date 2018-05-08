a060fb1ed2230c4944a490a899aaea57bc6d76c6
Jonathan Abantao <jonabantao@gmail.com>
2018-05-08 00:36:22 -0700

    Finishing CRUDing up the Users

--- W2D1/spring-boot-monolith/src/main/java/com/example/springbootmonolith/controllers/UsersController.java
+++ W2D1/spring-boot-monolith/src/main/java/com/example/springbootmonolith/controllers/UsersController.java
@@ -3,8 +3,10 @@ package com.example.springbootmonolith.controllers;
 import com.example.springbootmonolith.models.User;
 import com.example.springbootmonolith.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 
 @RestController
@@ -17,4 +19,31 @@ public class UsersController {
         return userRepository.findAll();
     }
 
+    @GetMapping("/users/{userId}")
+    public Optional<User> findUserById(@PathVariable Long userId) {
+        return userRepository.findById(userId);
+    }
+
+    @DeleteMapping("/users/{userId}")
+    public HttpStatus deleteUserById(@PathVariable Long userId) {
+        userRepository.deleteById(userId);
+        return HttpStatus.OK;
+    }
+
+    @PostMapping("/users")
+    public User createNewUser(@RequestBody User newUser) {
+        return userRepository.save(newUser);
+    }
+
+    @PatchMapping("/users/{userId}")
+    public User updateUserById(@PathVariable Long userId, @RequestBody User userRequest) {
+
+        User userFromDb = userRepository.findById(userId).get();
+
+        userFromDb.setUserName(userRequest.getUserName());
+        userFromDb.setFirstName(userRequest.getFirstName());
+        userFromDb.setLastName(userRequest.getLastName());
+
+        return userRepository.save(userFromDb);
+    }
 }
