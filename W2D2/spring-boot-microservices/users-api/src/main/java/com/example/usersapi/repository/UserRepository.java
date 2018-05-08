06fe4484d080bc9c0ad44d2bfd475c0beddbfe7e
Jonathan Abantao <jonabantao@gmail.com>
2018-05-08 15:30:27 -0700

    Complete users-api controller CRUD functionality

---
+++ W2D2/spring-boot-microservices/users-api/src/main/java/com/example/usersapi/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.usersapi.repository;
+
+import com.example.usersapi.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
