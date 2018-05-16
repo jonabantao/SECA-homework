becec0dd065936cc9b5b57b2379cfca5dd44d90e
Jonathan Abantao <jonabantao@gmail.com>
2018-05-16 00:43:39 -0700

    Add temporarily links to account balance component

--- W3D2/bank-of-react/src/components/AccountBalance.js
+++ W3D2/bank-of-react/src/components/AccountBalance.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const AccountBalance = ({ currentBalance }) => (
   <div>
+    <NavLink to="/account/home">Home</NavLink>
+    <NavLink to="/account/debits">Debits</NavLink>
+    <NavLink to="/account/credits">Credits</NavLink>
     Balance: ${currentBalance}
   </div>
 );
